40d8b1d5b1ad5a4061a641a3a6a02511326141b4
NGHT-Tur <104224519+NGHT-Tur@users.noreply.github.com>
2022-04-29 15:24:30 -0300

    29-04-2022

--- sketch.js
+++ sketch.js
@@ -1,34 +1,38 @@
 var golfinho, golfinhoNadando;
 var areia;
 var areia2;
+var areia3; 
+
+
 function preload(){
-  golfinhoNadando = loadAnimation("trex1.png","trex3.png","trex4.png");
+golfinhoNadando = loadAnimation("trex1.png","trex3.png","trex4.png");
 areia2= loadImage("ground2.png");
 }
 
 function setup(){
-  createCanvas(600,200);
-  golfinho = createSprite(50, 160, 20, 50);
-  golfinho.addAnimation("nadando", golfinhoNadando);
-  golfinho.scale = 0.5;
- areia = createSprite (200,180,600,20);
- areia.addImage(areia2);
- areia.x= areia.width/2;
-  bordas = createEdgeSprites();
+createCanvas(600,200);
+areia = createSprite (200,180,600,20);
+areia.addImage(areia2);
+areia.x= areia.width/2;
+areia3 = createSprite(0,190,400,10);
+areia3.visible = false;
+golfinho = createSprite(50, 160, 20, 50);
+golfinho.addAnimation("nadando", golfinhoNadando);
+golfinho.scale = 0.5;
+bordas = createEdgeSprites();
 }
 
 function draw(){
-  background("lightgrey");
-
-  if(keyDown("space")){
-    golfinho.velocityY = -10;
-  }
-  golfinho.velocityY += 1;
-  
-  golfinho.collide(areia);
+background("lightgrey");
+console.log (golfinho.y);
+if(keyDown("space")&& golfinho.y >= 150){
+golfinho.velocityY = -9;
+}
+golfinho.velocityY += 1; 
+golfinho.collide(areia3);
 areia.velocityX = -2;
 if(areia.x<0){
-  areia.x=areia.width/2;
+areia.x=areia.width/2;
 }
-  drawSprites();
+drawSprites();
 }
